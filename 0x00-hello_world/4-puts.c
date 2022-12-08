05040db0e08913a93dee87187ba229f0881e4ea7
Birhan <birhanyalew2019@gmail.com>
2022-12-08 15:14:15 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 
-
 /**
  * main - Entry point
  * 
@@ -9,6 +8,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
