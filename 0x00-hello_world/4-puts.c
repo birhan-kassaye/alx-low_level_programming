0d8773fa2266daaa67f2bde3b721beb358aee38d
Birhan <birhanyalew2019@gmail.com>
2022-12-08 15:08:56 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 
 
 /**
@@ -8,8 +8,7 @@
  */
 
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
-        return(0);
+	return (0);
 }
