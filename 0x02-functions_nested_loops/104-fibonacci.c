79492eb8e04a541f6ee79e7c1eaec0041d400b33
Birhan <birhanyalew2019@gmail.com>
2022-12-14 09:43:17 -0800

    16. My teacher gave me an A+

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <main.h>
+
+/**
+ * main - finds and prints the first 98 Fibonacci numbers
+ *
+ * Return: all returns 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	long int n1, n2, fn;
+
+	n1 = 1;
+	n2 = 2;
+	fn = 0;
+	printf("%ld, %ld", n1, n2)
+	for (i = 1; i < 98; i++)
+	{
+		fn = n1 + n2;
+		printf(", %ld", fn);
+		n1 = n2;
+		n2 = fn;
+	}
+	return (0);
+}
