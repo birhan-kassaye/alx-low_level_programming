02ee28b6cb73dd9c5ba57b6e8dce43d30fd792da
Birhan <birhanyalew2019@gmail.com>
2022-12-20 13:28:57 -0800

    5.  reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,18 +3,30 @@
 /**
  * rev_string - a function that reverses a string
  * @s: string
- * Return: returns 0
+ * Return: there is no return
  */
 
 void rev_string(char *s)
 {
-	int length = strlen(s);
-	int middle = length / 2;
-	char temp;
-	for (int i = 0; i < middle; i++)
+	int count, a, b;
+	char *str, temp;
+
+	count = 0;
+	while (count >= 0)
 	{
-		temp = s[i];
-		s[i] = s[length - i - 1];
-		s[length - i - 1] = temp;
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+	str = s;
+
+	for (a = 0; a < (count - 1); a++)
+	{
+		for (b = a + 1; b > 0; b--)
+		{
+			temp = *(str + b);
+			*(str + b) = *(str + (b - 1));
+			*(str + (b - 1)) = temp;
+		}
 	}
 }
