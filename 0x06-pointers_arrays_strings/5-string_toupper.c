973cb402fab8525a3b6e191fb729fddc90802340
Birhan <birhanyalew2019@gmail.com>
2022-12-22 13:08:09 -0800

    5. Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,11 +11,11 @@ char *string_toupper(char *s)
 	int count;
 
 	count = 0;
-	while (s[count] != '\0')
+	while (*(s + count) != '\0')
 	{
-		if ((s[count] >= 97) && (s[count] <= 122))
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
 		{
-			s[count] = s[count] - 32;
+			*(s + count) = *(s + count) - 32;
 		}
 		count++;
 	}
