cd82938109adbecb4ac6236241fecda4183602e9
Birhan <birhanyalew2019@gmail.com>
2022-12-22 06:57:20 -0800

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,9 +15,10 @@ int _strcmp(char *s1, char *s2)
 	res = 0;
 	while (res == 0)
 	{
-		if (s1[i] != '\0' && s2[i] != '\0')
+		if (s1[i] == '\0' && s2[i] == '\0')
 			break;
 		res = s1[i] - s2[i];
+		i++;
 	}
 	return (res);
 }
