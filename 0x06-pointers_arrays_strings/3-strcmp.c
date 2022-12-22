ac672bf2ace55afefd91d75de4c93fa66b12c9b7
Birhan <birhanyalew2019@gmail.com>
2022-12-22 06:28:54 -0800

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - a function that compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: returns substraction result
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, res;
+
+	i = 0;
+	res = 0;
+	while (res == 0)
+	{
+		if (s1[i] != '\0' && s2[i] != '\0')
+			break;
+		res = s1[i] - s2[i];
+	}
+	return (res);
+}
