c7de6f968d96b5f32e0c630fed163e89bf830847
Birhan <birhanyalew2019@gmail.com>
2022-12-22 04:47:06 -0800

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#inlcude "main.h"
+
+/**
+ * *_strncpy - a function that copies a string
+ * @dest: the destination of the file
+ * @src: the source of the file
+ * @n: the number of bytes used from the source
+ * Return: returns a pointer to the destination
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && *(src + i) != '\0'; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	for (; i < n; i++)
+	{
+		*(dest + i) = '\0';
+	}
+	return (dest);
+}
+
