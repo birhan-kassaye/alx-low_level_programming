de7984a5ca55949ebb3bfb9a08663916f96cc3b9
Birhan <birhanyalew2019@gmail.com>
2022-12-24 02:27:25 -0800

    Adding task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,9 @@
-nclude <stdio.h>
+#include <stdio.h>
 
+/**
+ * main - program that prints a[2] = 98
+ * Return: returns 0
+ */
 int main(void)
 {
 	int n;
@@ -15,10 +19,9 @@ int main(void)
 	 * - you are not allowed to modify p
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
-	 */   
-	  p[5] = 98;
-	  /* ...so that this prints 98\n */
-	  printf("a[2] = %d\n", a[2]);
-	    return (0);
+	 */
+	p[5] = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
-	
