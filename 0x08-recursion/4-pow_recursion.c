45d77898df02b5afd13cc344a37859a58bf12b19
Birhan <birhanyalew2019@gmail.com>
2023-01-04 15:02:23 -0800

    Adding task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - function that returns value of x raised to the power of y.
  * @x: the base.
  * @y: exponent.
- * Return: returns 0 
+ * Return: returns 0
  */
 
 int _pow_recursion(int x, int y)
@@ -21,3 +21,4 @@ int _pow_recursion(int x, int y)
 	{
 		return (x * _pow_recursion(x, y - 1));
 	}
+}
